0eedd97f1b723639c6817ac5a66d7f9c5c2b7fbd
Yasser RABI <yachironi@hotmail.com>
2014-12-27 17:14:33 +0100

    fix bug

--- src/Test/Train.java
+++ src/Test/Train.java
@@ -2,20 +2,21 @@ package Test;
 
 public class Train {
 	
+	private static int idGen =0;
 	private int id;     /* Identifiant du train */ 
 	private int taille; /* taile du train en nombre de trançon */
 	private int vMax;   /* vitesse maximale du train */ 
 	private EtatCourant etatTrain;
 	
-	public Train(int id, int taille, int vmax){
-		this.id = id;			/* besoin de faire un vérification pour l'id */
+	public Train(int taille, int vmax){
+		this.id = idGen++;		/* besoin de faire un vérification pour l'id */
 		if(taille <=0 || vmax <=0){
 			
 		}
 		this.taille = taille;
 		this.vMax =vmax;
 		/* a completer */ 
-		etatTrain = new EtatCourant(taille, );  /* à l'init v=0, et positête = taille ? */
+		//etatTrain = new EtatCourant(taille, );  /* à l'init v=0, et positête = taille ? */
 										/* on doit ajouter Rail aussi alors ? */
 	}
 	
