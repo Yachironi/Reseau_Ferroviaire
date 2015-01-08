6f04660af8cc134ee15a278c633d93b1733c5fc1
Yasser RABI <yachironi@hotmail.com>
2015-01-08 16:12:42 +0100

    Plusieur modification : Essentielement Element de regulation essentielement

--- src/reseauxFerroviaire/Jonction.java
+++ src/reseauxFerroviaire/Jonction.java
@@ -5,9 +5,23 @@ public abstract class Jonction {
 	private static int idGen = 0;
 	private int id; 
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public Jonction(){
 		id = idGen++;
 	}
 	
 	public abstract Rail getSuivant(Rail rail);
+
+	@Override
+	public String toString() {
+		return "Jonction [id=" + id + "]";
+	}
+	
 }
